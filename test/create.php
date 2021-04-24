1a9cdc679b2250d1138bde1c234a8da7e2171119
dasaint001 <elijahbamidele48@yahoo.com>
2021-04-24 10:57:24 +0100

    initial commit

---
+++ test/create.php
@@ -0,0 +1,116 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $code = $unit = "";
+$name_err = $code_err = $unit_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate course code
+    $input_code = trim($_POST["code"]);
+    if(empty($input_code)){
+        $code_err = "Please enter a course code.";     
+    } else{
+        $code = $input_code;
+    }
+    
+    // Validate course unit
+    $input_unit = trim($_POST["unit"]);
+    if(empty($input_unit)){
+        $unit_err = "Please enter the course unit.";     
+    } elseif(!ctype_digit($input_unit)){
+        $unit_err = "Please enter an integer value.";
+    } else{
+        $unit = $input_unit;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($code_err) && empty($unit_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO courses (name, code, unit) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_code, $param_unit);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_code = $code;
+            $param_unit = $unit;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add course record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Code</label>
+                            <textarea name="code" class="form-control <?php echo (!empty($code_err)) ? 'is-invalid' : ''; ?>"><?php echo $code; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $code_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Unit</label>
+                            <input type="text" name="unit" class="form-control <?php echo (!empty($unit_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $unit; ?>">
+                            <span class="invalid-feedback"><?php echo $unit_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
